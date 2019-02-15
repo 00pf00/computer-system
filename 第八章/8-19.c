ec985f7d41ecbd36f1effe8d85b6fc97094f9067
00pf00 <408445889@qq.com>
2019-02-15 19:11:01 +0800

    使用waitpid按照创建子进程的顺序来回收这些僵死子进程

---
+++ 第八章/8-19.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+#include <sys/errno.h>
+
+#define N 2
+
+int main() {
+    int status,i;
+    pid_t pid[N],retpid;
+    /*Parent create N children*/
+    for (i = 0 ; i < N ; i++){
+        if((pid[i] = fork()) == 0){
+            /*child*/
+            _exit(100+i);
+        }
+    }
+        /*Parent reaps N children in order*/
+        i = 0;
+        while((retpid = waitpid(pid[i++],&status,0)) > 0){
+            if(WIFEXITED(status)){
+                printf("child %d terminated normally with exit status =%d\n",retpid,WEXITSTATUS(status));
+            }else{
+                printf("child %d terminated abnormally\n",retpid);
+            }
+        }
+   /*The only normal termination is if there are more children*/
+   if(errno != ECHILD){
+       perror("waitpid error");
+   }
+   _exit(0);
+}
