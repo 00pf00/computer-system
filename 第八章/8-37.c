5bd9ef5ec8d0cf1f702bfd438a88658cd155fa44
00pf00 <408445889@qq.com>
2019-02-16 22:17:49 +0800

    正确解决信号不排队情况的信号处理

---
+++ 第八章/8-37.c
@@ -0,0 +1,59 @@
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <wait.h>
+#include "codes/csapp.h"
+
+void handler1(int sig);
+void handler2(int sig);
+int main()
+{
+    int i, n;
+    pid_t pid;
+    char buf[MAXBUF];
+
+    pid = getpid();
+    printf("self %d\n",pid);
+    if(signal(SIGCHLD,handler2) == SIG_ERR)
+        unix_error("signal error");
+    /*Parent create children*/
+    for (int i = 0; i < 3; ++i) {
+        if (Fork() == 0){
+            printf("Hello from child %d \n",(int)getpid());
+            exit(0);
+        }
+    }
+    /*Parent waits from terminal input and then processes it*/
+    if((n=read(STDERR_FILENO,buf, sizeof(buf))) < 0)
+        unix_error("read");
+    printf("Parent processing input \n");
+    while (1)
+        ;
+    exit(0);
+}
+
+void handler1(int sig)
+{
+    int olderrno = errno;
+    if((waitpid(-1,NULL,0)) < 0)
+        sio_error("waitpid error");
+    Sio_puts("Handler reaped child \n");
+    Sleep(1);
+    errno = olderrno;
+
+}
+
+void handler2(int sig)
+{
+    int  olderrno = errno;
+    while (waitpid(-1,NULL,0) > 0){
+        Sio_puts("Handler reaped child\n");
+    }
+    if(errno != ECHILD)
+        Sio_puts("waitpid error");
+    Sleep(1);
+    errno = olderrno;
+}
